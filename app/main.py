c48c3731bf0ee2c2f3f9b672435e717a94083bba
Mykyta <mitina1900@gmail.com>
2024-12-07 18:41:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"]
-        "column": error["column_number"]
-        "message": error["text"]
-        "name": error["code"]
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8"
     }
 
